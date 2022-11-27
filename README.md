c175c048ea1b3c55b25f4bed9299767a34b821cf
abbaslanbay <abbaslanbay@gmail.com>
2022-11-27 15:26:21 +0300

    Readme changed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# react-native-multi-language-example
+# React Native Multi Language Example
+
+I’ve been adding multi language to react native app
+
+## Documentation
+
+[Documentation](https://medium.com/@abbaslanbay/react-native-multi-language-app-960b1fa5e6b9)
--- Translations/resources/index.js
+++ Translations/resources/index.js
@@ -1,5 +1,5 @@
 export {default as en} from './en';
-export {default as ar} from './ar';
+export {default as es} from './es';
 export {default as fr} from './fr';
-export {default as de} from './de';
+export {default as eu} from './ru';
 export {default as tr} from './tr';
